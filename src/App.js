fcba4a4bda12b34d5f33ff8d45af599e52b37685
edmundxlin <edmundxlin@users.noreply.github.com>
2020-01-14 16:20:45 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,23 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div id="Frankenstein">
+        <h2>Frankenstein</h2>
+        <p id="fText">Frankenstein...</p>
+      </div>
+      
+      <div id="Jupiter">
+        <h2>Jupiter</h2>
+        
+      </div>
+      <img src={require('./tsunami.png')} width="100vw" height="100vh" alt="flood" id="tsunami" />
+      <div id="Government">
+        <h2>Government</h2>
+        
+      </div>
+      <img src={require('./images.png')} width="100vw" height="100vh" alt="cuffs" id="cuffs" />
+
+      <div id="Monster">
+        <h2>Monster</h2>
+        <div id="MonsterText">
+        <p>Frankenstein and the Monster's relationship is recursive in its nature. The more either seems to seek the destruction of the other, the farther away they seem.</p>
+        </div>
+      </div>
+        <div id="People">
+          <h2>Humans</h2>
+          <p id="jupHum">Humans... test</p>
+          <p id="AuthHum">Authority...</p>
+        </div>
+
     </div>
   );
 }

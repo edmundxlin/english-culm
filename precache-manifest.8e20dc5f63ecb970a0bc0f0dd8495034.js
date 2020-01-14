b44eff5e15d4e79f23346c8c656f9381f6259386
edmundxlin <edmundxlin@users.noreply.github.com>
2020-01-14 08:49:33 -0500

    Updates

---
+++ precache-manifest.8e20dc5f63ecb970a0bc0f0dd8495034.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a75bc3352cf19c87f8c0f556829ca19",
+    "url": "/english-culm/index.html"
+  },
+  {
+    "revision": "5aa60b7868fc516cf62c",
+    "url": "/english-culm/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "efeb9439f2cc368f655c",
+    "url": "/english-culm/static/js/2.ee753fa8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/english-culm/static/js/2.ee753fa8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5aa60b7868fc516cf62c",
+    "url": "/english-culm/static/js/main.eb29a0fc.chunk.js"
+  },
+  {
+    "revision": "d762041a900c9666a1b6",
+    "url": "/english-culm/static/js/runtime-main.e9ec541c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/english-culm/static/media/logo.5d5d9eef.svg"
+  }
+]);

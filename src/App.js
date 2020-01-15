4073a13e21a4b6a4fd251a221bed8f71143adbe0
edmundxlin <edmundxlin@users.noreply.github.com>
2020-01-15 02:13:39 -0500

    update

--- src/App.js
+++ src/App.js
@@ -11,17 +11,21 @@ function App() {
         <h2>Frankenstein</h2>
         <p id="fText">Frankenstein...</p>
       </div>
+      <img src={require('./shot.png')} width="120vw" height="80vh" alt="gun" id="shot" />
+      <img src={require('./download.png')} width="100vw" height="100vh" alt="cuffs" id="fists" />
       
       <div id="Jupiter">
         <h2>Jupiter</h2>
         
       </div>
       <img src={require('./tsunami.png')} width="100vw" height="100vh" alt="flood" id="tsunami" />
+      <img src={require('./lightning.png')} width="100vw" height="100vh" alt="ln" id="lightning" />
       <div id="Government">
         <h2>Government</h2>
-        
+        <p>Governments enforce the law on the citizens, yet when NA countries are inclined to  punish instead of rehabilitate, </p>
       </div>
       <img src={require('./images.png')} width="100vw" height="100vh" alt="cuffs" id="cuffs" />
+      <img src={require('./gun.png')} width="100vw" height="80vh" alt="gun" id="gun" />
 
       <div id="Monster">
         <h2>Monster</h2>
@@ -31,8 +35,8 @@ function App() {
       </div>
         <div id="People">
           <h2>Humans</h2>
-          <p id="jupHum">Humans... test</p>
-          <p id="AuthHum">Authority...</p>
+          <p id="jupHum">And the humans continued their sacriligeous ways, despite the punishment from the Gods.</p>
+          <p id="AuthHum">The recidivism of criminals can be correlated to the harsh negligence from Government officials.</p>
         </div>
 
     </div>
